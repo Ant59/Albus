3146f462c99960df501adc534eb160412d8d0953
Ant59 <antonyderham@gmail.com>
2011-02-14 00:00:28 +0000

    Version 2.0 ready. MySQL custom support added. Stripped crap out.

--- src/net/districtmine/ant59/albus/Albus.java
+++ src/net/districtmine/ant59/albus/Albus.java
@@ -1,25 +1,4 @@
-/*
- * Copyright (C) 2011 <silence@immortal-forces.net>
- *
- * This file is part of the Bukkit plugin SMFWhite.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307, USA.
- */
-
-// Modifications Copyright (C) 2011 Antony <admin@districtmine.net>
+// Copyright (C) 2011 Antony Derham <admin@districtmine.net>
 
 package net.districtmine.ant59.albus;
 
@@ -31,7 +10,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Properties;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -49,10 +27,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
 
 public class Albus extends JavaPlugin {
-	// Logger
-	public static final Logger log = Logger.getLogger("Minecraft"); // Logger
-
-	// Constants
+	public static final Logger log = Logger.getLogger("Minecraft");
+	
 	private final String PROP_KICKMESSAGE = "kick-message";
 	private final String PROP_WHITELIST_ADMINS = "whitelist-admins";
 	private final String PROP_DISABLE_LIST = "disable-list-command";
@@ -61,10 +37,12 @@ public class Albus extends JavaPlugin {
 	private final String PROP_MYSQL_USER = "mysql-user";
 	private final String PROP_MYSQL_PASS = "mysql-pass";
 	private final String PROP_MYSQL_DB = "mysql-db";
-	private final String PROP_MYSQL_PREFIX = "mysql-prefix";
-	private final String PROP_SMF_ALLOWED_GROUPS = "smf-groups";
-	// private final String FILE_WHITELIST = "whitelist.txt";
-	private final String FILE_CONFIG = "albus.properties";
+	private final String PROP_MYSQL_USERS_TABLE = "mysql-table";
+	private final String PROP_GROUP_FIELD = "group-field";
+	private final String PROP_USERNAME_FIELD = "username-field";
+	private final String PROP_ALLOWED_GROUP_IDS = "allowed-group-ids";
+	private final String PROP_RELOAD_PERIOD = "reload-period";
+	private final String configurationFile = "albus.properties";
 
 	// Plugin
 	private String name;
@@ -72,14 +50,13 @@ public class Albus extends JavaPlugin {
 	private PluginDescriptionFile pdfFile;
 
 	// Attributes
-	private final WLPlayerListener m_PlayerListner = new WLPlayerListener(this);
-	private Timer m_Timer;
-	private File m_Folder;
-	private ArrayList<String> m_WhitelistAdmins;
-	private ArrayList<String> m_WhitelistAllow;
-	private String m_KickMessage;
+	private final AlbusListener playerListner = new AlbusListener(this);
+	private Timer timer = new Timer(true);
+	private File albusFolder;
+	private ArrayList<String> allowed;
+	private String kickMessage;
 	private boolean m_IsWhitelistActive;
-	private boolean m_IsListCommandDisabled;
+	private boolean isActive;
 
 	// Database
 	private MySQL sql;
@@ -89,132 +66,91 @@ public class Albus extends JavaPlugin {
 			ClassLoader cLoader) {
 		super(pluginLoader, instance, desc, folder, plugin, cLoader);
 
-		m_Folder = folder;
-		m_KickMessage = "";
-		m_WhitelistAdmins = new ArrayList<String>();
-		m_WhitelistAllow = new ArrayList<String>();
+		albusFolder = folder;
+		kickMessage = "";
+		allowed = new ArrayList<String>();
 		m_IsWhitelistActive = true;
-		m_IsListCommandDisabled = false;
+		isActive = false;
 	}
 
-	public void onEnable() {
-		// Register our events
-		PluginManager pm = getServer().getPluginManager();
+	class ReloaderTask extends TimerTask {
+        public void run() {
+        	loadWhitelistSettings();
+        }
+    }
 
-		pm.registerEvent(Event.Type.PLAYER_LOGIN, m_PlayerListner,
-				Priority.Low, this);
-		// pm.registerEvent(Event.Type.PLAYER_COMMAND, m_PlayerListner,
-		// Priority.Monitor, this);
+	public void onEnable() {
+		getServer().getPluginManager().registerEvent(Event.Type.PLAYER_LOGIN, playerListner, Priority.Low, this);
+		getServer().getPluginManager().registerEvent(Event.Type.PLAYER_COMMAND, playerListner, Priority.Monitor, this);
 
 		this.pdfFile = this.getDescription();
 		this.name = this.pdfFile.getName();
 		this.version = this.pdfFile.getVersion();
 
 		// Create folders and files
-		if (!m_Folder.exists()) {
+		if (!albusFolder.exists()) {
 			consoleLog("Config folder missing, creating...");
-			m_Folder.mkdir();
+			albusFolder.mkdir();
 			consoleLog("Folder created");
 		}
-		/*
-		 * File fWhitelist = new File(m_Folder.getAbsolutePath() +
-		 * File.separator + FILE_WHITELIST); if (!fWhitelist.exists()) {
-		 * System.out.print("Whitelist: Whitelist is missing, creating..."); try
-		 * { fWhitelist.createNewFile(); System.out.println("done."); } catch
-		 * (IOException ex) { System.out.println("failed."); } }
-		 */
 
-		File fConfig = new File(m_Folder.getAbsolutePath() + File.separator
-				+ FILE_CONFIG);
+		File fConfig = new File(albusFolder.getAbsolutePath() + File.separator + configurationFile);
 		if (!fConfig.exists()) {
 			consoleLog("Config is missing, creating...");
 			try {
 				fConfig.createNewFile();
 				Properties propConfig = new Properties();
-				propConfig.setProperty(PROP_KICKMESSAGE,
-						"Sorry, you are not on the whitelist!");
-				propConfig.setProperty(PROP_WHITELIST_ADMINS,
-						"Name1,Name2,Name3");
+				propConfig.setProperty(PROP_KICKMESSAGE, "Sorry, you are not on the whitelist!");
+				propConfig.setProperty(PROP_WHITELIST_ADMINS, "Name1,Name2,Name3");
 				propConfig.setProperty(PROP_DISABLE_LIST, "false");
 				propConfig.setProperty(PROP_MYSQL_HOST, "localhost");
 				propConfig.setProperty(PROP_MYSQL_PORT, "3306");
 				propConfig.setProperty(PROP_MYSQL_USER, "user");
 				propConfig.setProperty(PROP_MYSQL_PASS, "pass");
 				propConfig.setProperty(PROP_MYSQL_DB, "forum");
-				propConfig.setProperty(PROP_MYSQL_PREFIX, "smf_");
-				propConfig.setProperty(PROP_SMF_ALLOWED_GROUPS, "1,2,3");
-				BufferedOutputStream stream = new BufferedOutputStream(
-						new FileOutputStream(fConfig.getAbsolutePath()));
-				propConfig.store(stream,
-						"Auto generated config file, please modify");
-				consoleLog("Config created");
+				propConfig.setProperty(PROP_MYSQL_USERS_TABLE, "smf_members");
+				propConfig.setProperty(PROP_GROUP_FIELD, "group_id");
+				propConfig.setProperty(PROP_USERNAME_FIELD, "name");
+				propConfig.setProperty(PROP_ALLOWED_GROUP_IDS, "1,2,3");
+				propConfig.setProperty(PROP_RELOAD_PERIOD, "300");
+				BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(fConfig.getAbsolutePath()));
+				propConfig.store(stream, "Automatically generated config file");
+				consoleLog("Configuration created");
 			} catch (IOException ex) {
-				consoleWarning("Failed to create config");
+				consoleWarning("Configuration file creation failure");
 			}
 		}
-		
-		class ReloaderTask extends TimerTask {
-	        public void run() {
-	        	loadWhitelistSettings();
-	        }
-	    }
-		
-		// Start reloading timer
-		m_Timer = new Timer(true);
-		m_Timer.schedule(new ReloaderTask(), 0, 300000);
 
 		consoleLog("Enabled!");
 	}
 
 	public void onDisable() {
-		m_Timer.cancel();
-		m_Timer.purge();
-		m_Timer = null;
+		timer.cancel();
+		timer.purge();
+		timer = null;
 		consoleLog("Goodbye!");
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd,
-			String commandLabel, String[] args) {
-		if (!sender.isOp())
-			return true; // only whitelist admins are allowed to use /whitelist
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 
 		if (args.length < 1) {
 			return false;
 		}
 		if (args[0].compareToIgnoreCase("help") == 0) {
-			sender.sendMessage(ChatColor.YELLOW + "Commands:");
-			sender.sendMessage(ChatColor.YELLOW
-					+ "/whitelist reload  (reloads the whitelist and settings)");
-			sender.sendMessage(ChatColor.YELLOW
-					+ "/whitelist on|off  (actives/deactivates whitelist)");
-			sender.sendMessage(ChatColor.YELLOW
-					+ "/whitelist list  (list whitelist entries)");
+			sender.sendMessage(ChatColor.YELLOW + "/albus reload  (force an update of the whitelist)");
+			sender.sendMessage(ChatColor.YELLOW	+ "/albus list  (lists everyone whitelisted)");
 			return true;
 		}
 		if (args[0].compareToIgnoreCase("reload") == 0) {
-			if (reloadSettings())
-				sender.sendMessage(ChatColor.GREEN
-						+ "Settings and whitelist reloaded");
+			if (loadWhitelistSettings())
+				sender.sendMessage(ChatColor.GREEN + "Settings and whitelist reloaded");
 			else
-				sender.sendMessage(ChatColor.RED
-						+ "Could not reload whitelist...");
+				sender.sendMessage(ChatColor.RED + "Could not reload...");
 			return true;
 		}
-		if (args[0].compareToIgnoreCase("on") == 0) {
-			setWhitelistActive(true);
-			sender.sendMessage(ChatColor.GREEN + "Whitelist activated!");
-			return true;
-		}
-		if (args[0].compareToIgnoreCase("off") == 0) {
-			setWhitelistActive(false);
-			sender.sendMessage(ChatColor.RED + "Whitelist deactivated!");
-			return true;
-		}
-		if (args[0].compareToIgnoreCase("list") == 0
-				&& !isListCommandDisabled()) {
-			sender.sendMessage(ChatColor.YELLOW + "Players on whitelist: "
-					+ ChatColor.GRAY + getFormatedAllowList());
+		if (args[0].compareToIgnoreCase("list") == 0 && !isListCommandDisabled()) {
+			sender.sendMessage(ChatColor.YELLOW + "Allowed players: " + ChatColor.GRAY + getFormatedAllowList());
 			return true;
 		}
 		return false;
@@ -223,50 +159,36 @@ public class Albus extends JavaPlugin {
 	public boolean loadWhitelistSettings() {
 		consoleLog("Trying to load whitelist and settings...");
 		try {
-			// 1. Load fWhitelist.properties
+			// Load albus.properties
 			Properties propConfig = new Properties();
-			BufferedInputStream stream = new BufferedInputStream(
-					new FileInputStream(m_Folder.getAbsolutePath()
-							+ File.separator + FILE_CONFIG));
+			BufferedInputStream stream = new BufferedInputStream(new FileInputStream(albusFolder.getAbsolutePath() + File.separator + configurationFile));
 			propConfig.load(stream);
-			m_KickMessage = propConfig.getProperty(PROP_KICKMESSAGE);
-			if (m_KickMessage == null) {
-				m_KickMessage = "";
+			timer.schedule(new ReloaderTask(), 0, Integer.parseInt(propConfig.getProperty(PROP_RELOAD_PERIOD))*1000);
+			kickMessage = propConfig.getProperty(PROP_KICKMESSAGE);
+			if (kickMessage == null) {
+				kickMessage = "";
 			}
-			m_WhitelistAdmins.clear();
-			String rawAdminList = propConfig.getProperty(PROP_WHITELIST_ADMINS);
-			if (rawAdminList != null) {
-				String[] admins = rawAdminList.split(",");
-				if (admins != null) {
-					m_WhitelistAdmins.addAll(Arrays.asList(admins));
-				}
-			}
-			String rawDisableListCommand = propConfig
-					.getProperty(PROP_DISABLE_LIST);
+			String rawDisableListCommand = propConfig.getProperty(PROP_DISABLE_LIST);
 			if (rawDisableListCommand != null) {
-				m_IsListCommandDisabled = Boolean
+				isActive = Boolean
 						.parseBoolean(rawDisableListCommand);
 			}
 
-			// 2. Load database configuration and connect...
+			// Load database configuration and connect...
 			this.sql = new MySQL(this, propConfig.getProperty(PROP_MYSQL_USER),
 					propConfig.getProperty(PROP_MYSQL_PASS),
 					propConfig.getProperty(PROP_MYSQL_HOST),
 					propConfig.getProperty(PROP_MYSQL_PORT),
 					propConfig.getProperty(PROP_MYSQL_DB));
 
-			// 3. Load whitelist from SMF database
-			m_WhitelistAllow.clear();
+			// Load whitelist from database
+			allowed.clear();
 
 			ResultSet rs;
-			rs = sql.trySelect("select member_name from "
-					+ propConfig.getProperty(PROP_MYSQL_PREFIX)
-					+ "members where id_group IN("
-					+ propConfig.getProperty(PROP_SMF_ALLOWED_GROUPS) + ")");
-
+			rs = sql.trySelect("SELECT " + propConfig.getProperty(PROP_USERNAME_FIELD) + " from " + propConfig.getProperty(PROP_MYSQL_USERS_TABLE) + " WHERE " + propConfig.getProperty(PROP_USERNAME_FIELD) + " IN(" + propConfig.getProperty(PROP_ALLOWED_GROUP_IDS) + ")");
 			while (rs.next() != false) {
-				String user = rs.getString("member_name");
-				m_WhitelistAllow.add(user);
+				String user = rs.getString(propConfig.getProperty(PROP_USERNAME_FIELD));
+				allowed.add(user);
 			}
 
 			consoleLog("Whitelist Loaded");
@@ -277,26 +199,8 @@ public class Albus extends JavaPlugin {
 		return true;
 	}
 
-	/*
-	 * public boolean saveWhitelist() { try { BufferedWriter writer = new
-	 * BufferedWriter( new FileWriter( (m_Folder.getAbsolutePath() +
-	 * File.separator + FILE_WHITELIST))); for (String player :
-	 * m_WhitelistAllow) { writer.write(player); writer.newLine(); }
-	 * writer.close(); } catch (Exception ex) { consoleWarning(ex.getMessage());
-	 * return false; } return true; }
-	 */
-
-	public boolean isAdmin(String playerName) {
-		for (String admin : m_WhitelistAdmins) {
-			if (admin.compareToIgnoreCase(playerName) == 0) {
-				return true;
-			}
-		}
-		return false;
-	}
-
 	public boolean isOnWhitelist(String playerName) {
-		for (String player : m_WhitelistAllow) {
+		for (String player : allowed) {
 			if (player.compareToIgnoreCase(playerName) == 0) {
 				return true;
 			}
@@ -304,28 +208,13 @@ public class Albus extends JavaPlugin {
 		return false;
 	}
 
-	/*
-	 * public boolean addPlayerToWhitelist(String playerName) { if
-	 * (!isOnWhitelist(playerName)) { m_WhitelistAllow.add(playerName); return
-	 * saveWhitelist(); } return false; }
-	 * 
-	 * public boolean removePlayerFromWhitelist(String playerName) { for (int i
-	 * = 0; i < m_WhitelistAllow.size(); i++) { if
-	 * (playerName.compareToIgnoreCase(m_WhitelistAllow.get(i)) == 0) {
-	 * m_WhitelistAllow.remove(i); return saveWhitelist(); } } return false; }
-	 */
-
-	public boolean reloadSettings() {
-		return loadWhitelistSettings();
-	}
-
 	public String getKickMessage() {
-		return m_KickMessage;
+		return kickMessage;
 	}
 
 	public String getFormatedAllowList() {
 		String result = "";
-		for (String player : m_WhitelistAllow) {
+		for (String player : allowed) {
 			if (result.length() > 0) {
 				result += ", ";
 			}
@@ -343,19 +232,8 @@ public class Albus extends JavaPlugin {
 	}
 
 	public boolean isListCommandDisabled() {
-		return m_IsListCommandDisabled;
+		return isActive;
 	}
-
-	/*public boolean needReloadWhitelist() {
-		if (m_Watcher != null)
-			return m_Watcher.wasFileModified();
-		return false;
-	}
-
-	public void resetNeedReloadWhitelist() {
-		if (m_Watcher != null)
-			m_Watcher.resetFileModifiedState();
-	}*/
 
 	public void consoleLog(String msg) {
 		log.info("[" + name + "] v" + version + " - " + msg);
